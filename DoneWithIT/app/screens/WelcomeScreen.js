2b81bc93d117d336e7f6ec108faa5c3f0dd5cb63
Anik2069 <anikislam668@gmail.com>
2024-07-27 01:04:04 +0600

    first commit

---
+++ DoneWithIT/app/screens/WelcomeScreen.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Image, ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <SafeAreaView
+            style={styles.background}
+           >
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require("../../assets/anik.jpg")}></Image>
+                <Text>Login Here</Text>
+            </View>
+
+            <View style={styles.loginButton}></View>
+            <View style={styles.regButton}></View>
+            <Text>@ANIK</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    loginButton: {
+        backgroundColor: "green",
+        width: "100%",
+        height: 50
+    },
+    logo: {
+        // position: "absolute",
+        // top: 70,
+        width: 100,
+        height: 100
+    },
+    regButton: {
+        backgroundColor: "red",
+        width: "100%",
+        height: 50
+    },
+    logoContainer: {
+        position: "absolute",
+        top: 70,
+        alignItems: "center"
+    }
+})
+
+export default WelcomeScreen;
