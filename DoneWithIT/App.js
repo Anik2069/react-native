a61112461f595f37aa63e3f519abb383f43be56a
Anik2069 <anikislam668@gmail.com>
2024-07-28 00:31:04 +0600

    cc

--- DoneWithIT/App.js
+++ DoneWithIT/App.js
@@ -1,19 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
-export default function App() {
+function Tweets({ navigation }) {
   return (
-    <WelcomeScreen />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      {/* <Button title='View Tweet' onPress={() => navigation.navigate("TweetsDetails")} /> */}
+      <Link />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
+
+function TweetsDetails({ route }) {
+  //useRoute
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen {route.params.id}</Text>
+
+    </View>
+  );
+}
+
+function Link() {
+  const navigation = useNavigation();
+  return (
+    <Button title='click' onPress={() => navigation.navigate("TweetsDetails", { id: 10 })} />
+  );
+}
+
+
+
+function Account({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Account</Text>
+      {/* <Button title='View Tweet' onPress={() => navigation.navigate("TweetsDetails")} /> */}
+      <Link />
+    </View>
+  );
+}
+
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {/* stack navigation */}
+      {/* <Stack.Navigator screenOptions={{
+        headerStyle: { backgroundColor: "dodgerblue" },
+        headerTintColor: "white",
+      }}>
+        <Stack.Screen name='Tweets' component={Tweets}
+          options={{
+            headerStyle: { backgroundColor: "tomato" },
+            headerTintColor: "white",
+            // headerShown: false
+          }}
+        />
+        <Stack.Screen name='TweetsDetails' options={({ route }) => ({ title: route.params.id })} component={TweetsDetails} />
+      </Stack.Navigator> */}
+
+
+      {/* Tab navigation */}
+
+      <Tab.Navigator>
+        <Tab.Screen name='Tweets' component={Tweets} />
+        <Tab.Screen name='Account' component={Account} />
+      </Tab.Navigator>
+    </NavigationContainer >
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     // alignItems: 'center',
+//     // justifyContent: 'center',
+//   },
+// });
