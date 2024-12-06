ace5f3c6f739336ec74fa6e376b803afa661425f
Anik2069 <anikislam668@gmail.com>
2024-12-06 21:12:48 +0600

    Fresh Setup WIth Native wind

--- uber/app/_layout.tsx
+++ uber/app/_layout.tsx
@@ -1,3 +1,5 @@
+import "../global.css"
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -28,12 +30,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  )
 }
