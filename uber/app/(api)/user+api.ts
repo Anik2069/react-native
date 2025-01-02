fc44136866d1f938c2d50097315a2033ff7188a0
Md Anik Islam <anikislam668@gmail.com>
2025-01-02 16:25:38 +0600

    cc

--- uber/app/(api)/user+api.ts
+++ uber/app/(api)/user+api.ts
@@ -1,11 +1,27 @@
 import { neon } from "@neondatabase/serverless";
+import { data } from "../constants";
 
-const sql = neon(`${process.env.DATABASE_URL}`);
+export async function post(request: Request) {
+  try {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const { name, email, clerkId } = await request.json();
 
-const posts = await sql("SELECT * FROM posts");
+    if (!name || !email || !clerkId) {
+      return Response.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
 
-export async function post(request) {
-    
+    const response = await sql(
+      `insert into users (name, email, clerk_id) values ('${name}', '${email}', '${clerkId}')`
+    );
+
+    return new Response(JSON.stringify({ data: response }), { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return new Response({ error: error }, { status: 500 });
+  }
 }
 
 // See https://neon.tech/docs/serverless/serverless-driver
