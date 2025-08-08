5e249cf605819a4d81a82f0687e35531818dee9f
Md Anik Islam <anikislam668@gmail.com>
2025-08-09 03:12:56 +0600

    api path change

---
+++ mobile_movie_app/services/useFetch.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunctions: () => Promise<T>, autoFetch = true) => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            const data = await fetchFunctions();
+        
+            setData(data);
+        } catch (error) {
+            setError(error as Error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+
+
+
+    const reset = () => {
+        setData(null);
+        setLoading(false);
+        setError(null);
+    };
+
+    useEffect(() => {
+        if (autoFetch) {
+            fetchData();
+        }
+    }, []);
+
+
+    return {
+        data,
+        loading,
+        error,
+        refatch: fetchData,
+        reset,
+    };
+};
+
+export default useFetch;
