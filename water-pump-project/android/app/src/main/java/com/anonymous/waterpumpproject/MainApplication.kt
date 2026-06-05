fbc371a25a20f2bfffd6acdbfb6b0173ca4c22f3
RaselAhmed <raselahamed613@gmail.com>
2026-06-06 04:15:40 +0600

    required changes at 06 jun 2026

--- water-pump-project/android/app/src/main/java/com/anonymous/waterpumpproject/MainApplication.kt
+++ water-pump-project/android/app/src/main/java/com/anonymous/waterpumpproject/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.waterpumpproject
+package com.waterpumpmonitor
 
 import android.app.Application
 import android.content.res.Configuration
--- water-pump-project/app/_layout.tsx
+++ water-pump-project/app/_layout.tsx
@@ -2,9 +2,10 @@ import "../global.css";
 
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';;
+import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { StatusBar } from 'expo-status-bar'; // status icon color error
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -26,14 +27,15 @@ export default function RootLayout() {
   }
 
   return (
-
-    <Stack >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-
-
+    <>
+      <Stack >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(root)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="dark" />
+    </>
   );
 }
+
